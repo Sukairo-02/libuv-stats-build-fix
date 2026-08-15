5c19ec884bd3da0933d7e18d0ab0d18de12a77a0
aquapi <aquaplmc@gmail.com>
2026-08-15 12:35:41 +0700

    initial

--- build.rs
+++ build.rs
@@ -1,3 +1,11 @@
 fn main() {
   napi_build::setup();
+
+  if cfg!(windows) {
+    println!(
+      "cargo:rustc-link-search=native={}",
+      std::env::var("NODE_DIR").unwrap()
+    );
+    println!("cargo:rustc-link-lib=dylib=node");
+  }
 }
