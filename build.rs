3c2454d38c0532b2a78333370b5c23a9ec9897df
aquapi <aquaplmc@gmail.com>
2026-08-16 22:38:37 +0700

    initial

--- build.rs
+++ build.rs
@@ -2,10 +2,12 @@ fn main() {
   napi_build::setup();
 
   if cfg!(windows) {
-    println!(
-      "cargo:rustc-link-search=native={}",
-      std::env::var("NODE_DIR").unwrap()
-    );
+    let node_lib_dir = std::path::PathBuf::from(std::env::var("NODE_LIB_DIR").expect(
+      "NODE_LIB_DIR must point to the target-arch directory containing
+              node.lib",
+    ));
+
+    println!("cargo:rustc-link-search=native={}", node_lib_dir.display());
     println!("cargo:rustc-link-lib=dylib=node");
   }
 }
