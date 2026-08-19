1ccb8b0bbbda5a19838ab350018d94de6c03f25e
Sukairo-02 <sreka9056@gmail.com>
2026-08-20 00:50:28 +0300

    Windows build fix

--- build.rs
+++ build.rs
@@ -1,12 +1,13 @@
 fn main() {
   napi_build::setup();
 
-  if cfg!(windows) {
+  if std::env::var("CARGO_CFG_TARGET_OS").as_deref() == Ok("windows") {
     let node_lib_dir = std::path::PathBuf::from(std::env::var("NODE_LIB_DIR").expect(
       "NODE_LIB_DIR must point to the target-arch directory containing
               node.lib",
     ));
 
+    println!("cargo:rerun-if-env-changed=NODE_LIB_DIR");
     println!("cargo:rustc-link-search=native={}", node_lib_dir.display());
     println!("cargo:rustc-link-lib=dylib=node");
   }
